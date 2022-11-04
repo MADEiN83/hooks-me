c6be3e5310dce217605a7410ce655516c20e7d61
MADEiN83 <anthony.distefano@hotmail.fr>
2022-11-04 15:18:28 +0100

    feat: add example file for `useSessionStorage` hook

---
+++ examples/useSessionStorage.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from "react";
+import { useSessionStorage } from "../lib/esm";
+
+const Component: FC = () => {
+  const [word, setWord, clearWord] = useSessionStorage("word", "famous");
+
+  return (
+    <>
+      <div>Hooks me, I'm {word}</div>
+      <button onClick={() => setWord("very famous")}>Set another word</button>
+      <button onClick={() => clearWord()}>Clear</button>
+    </>
+  );
+};
+
+export default Component;
